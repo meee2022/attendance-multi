c3728c4708b314851de642530c5ee5b4b5f0e79f
Meee87 <eng.mohamed19871120@gmail.com>
2026-04-05 01:22:38 +0300

    done

--- convex/tardiness.ts
+++ convex/tardiness.ts
@@ -1,6 +1,6 @@
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
-import { Id } from "./_generated/dataModel";
+import type { Id } from "./_generated/dataModel";
 
 // Get tardy students for a specific date
 export const getLatesByDate = query({
