ad5012d9a45768f72b259808bf4c1d6529cad757
Meee87 <eng.mohamed19871120@gmail.com>
2026-09-09 21:46:01 +0300

    Remove temporary design review fixtures

--- .design-qa/lib/SchoolContext.tsx
+++
@@ -1,1 +0,0 @@
-export const useSchool=()=>({school:{_id:'school',name:'مدرسة التجربة للتعليم الثانوي',code:'DEMO'},setSchool:()=>{},isLoading:false}); export const SchoolProvider=({children})=>children;
